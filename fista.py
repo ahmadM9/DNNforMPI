0f59c4f4d32452fcef15b974f47a985b4e88876b
ahmadM9 <ahmad.mmd26@gmail.com>
2021-06-25 21:01:31 +0200

    Add test case

--- fista.py
+++ fista.py
@@ -2,6 +2,8 @@ __author__ = "Ahmad Mohammad"
 
 import numpy as np
 from numpy import linalg as LA
+import scipy.io
+import matplotlib.pyplot as plt
 
 
 def calculate_lip_constant(A):
@@ -14,8 +16,7 @@ def prox(u, alpha_):
 
 
 def compute_gradient(A, b, y):
-    return np.dot(np.dot(A.transpose(), A), y)\
-           - np.dot(A.transpose(), b)
+    return np.dot(np.dot(A.transpose(), A), y) - np.dot(A.transpose(), b)
 
 
 class Fista:
@@ -70,7 +71,77 @@ class Fista:
         return x_next
 
 
+def get_voltage_signals():
+    freq = scipy.io.loadmat('./Datasets/FreqVoltages40x40.mat')
+
+    xcoil = freq.get('f1')
+    ycoil = freq.get('f2')
+
+    return xcoil, ycoil
 
 
+def get_system_matrices():
+    sysmat = scipy.io.loadmat('./Datasets/Vessel_System2.mat')
+
+    Af = sysmat.get('Af')
+    Bf = sysmat.get('Bf')
+
+    return Af, Bf
 
 
+def preprocess(xcoil, ycoil, af, bf):
+    af = af.reshape(817, -1)
+    af = np.concatenate((af.real, af.imag), axis=0)
+    bf = bf.reshape(817, -1)
+    bf = np.concatenate((bf.real, bf.imag), axis=0)
+
+    sys_ges = np.concatenate((af, bf), axis=0)
+
+    x = xcoil[10, :]
+    y = ycoil[10, :]
+
+    x = np.concatenate((x.real, x.imag))
+    y = np.concatenate((y.real, y.imag))
+
+    coil_ges = np.concatenate((x, y))
+
+    return sys_ges, coil_ges
+
+
+def get_image():
+    imgs = scipy.io.loadmat('./Datasets/Vessel40x40.mat')
+    imgs_all = imgs.get('images_all')
+    img = imgs_all[10]
+
+    return img
+
+
+def plot_results(x, img):
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(x)
+    plt.title('Reconstructed Image')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(img)
+    plt.title('Original Image')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    xcoil, ycoil = get_voltage_signals()
+    Af, Bf = get_system_matrices()
+
+    A, b = preprocess(xcoil, ycoil, Af, Bf)
+
+    fista = Fista()
+    x = fista.fista(A, b)
+    x = x.reshape(40, 40)
+
+    img = get_image()
+
+    plot_results(x, img)
+
+    # Calculate the difference
+    print(np.max(np.abs(img - x)))
