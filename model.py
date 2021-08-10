b7bbcc336bf63d6c1a229f0931408e3aa20f7027
ahmadM9 <ahmad.mmd26@gmail.com>
2021-08-10 13:03:47 +0200

    Build tensorflow model

---
+++ model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+
+def denoise_network():
+
+    inpt = tf.keras.Input(shape=(None, None, 1))
+
+    x = tf.keras.layers.Conv2D(8, 3, padding='same')(inpt)
+    x = tf.keras.activations.relu(x)
+
+    for i in range(3):
+        x = tf.keras.layers.Conv2D(8, 3, padding='same')(x)
+        x = tf.keras.activations.relu(x)
+
+    x = tf.keras.layers.Conv2D(1, 3, padding='same')(x)
+    x = tf.keras.layers.Subtract()([inpt, x])
+
+    model = tf.keras.Model(inputs=inpt, outputs=x)
+
+    return model
+
+
+
+
+
+
+
+
