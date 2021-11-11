01ddafdd47bfc3c853834d1f69892ddd32215a15
ahmadM9 <ahmad.mmd26@gmail.com>
2021-11-11 14:40:55 +0100

    Seperate Fista from the DcNN Fista

--- DcNN_fista.py
+++ DcNN_fista.py
@@ -1,19 +1,20 @@
-__author__ = "Ahmad Mohammad"
-
+from utitlities import calculate_lip_constant, compute_gradient, scaled_denoiser, compute_function_error
 import numpy as np
-from utitlities import calculate_lip_constant, compute_gradient, prox, compute_function_error
 
 
-class Fista:
+class DcNN_Fista:
     """
-    Fast iterative shrinking/thresholding algorithm
+    Fast iterative shrinking/thresholding algorithm with the help of
+    an image denoiser as an imaging prior
     """
 
-    def __init__(self, lambda_, iterations):
-        self.lambda_ = lambda_
+    def __init__(self, iterations, scaler, path_to_model, lambda_):
         self.iterations = iterations
+        self.scaler = scaler
+        self.path_to_model = path_to_model
+        self.lambda_ = lambda_
 
-    def fista(self, A, b, original_img, lip_constant):
+    def dcnn_fista(self, A, b, original_img, lip_constant):
 
         (n_samples, n_features) = A.shape
         n_kernels = int(n_features / n_samples)
@@ -33,18 +34,17 @@ class Fista:
         tau_old = 1.0
         tau_new = 0.0
 
-        patience = 20
+        patience = 30
         previous_error = 10 ** 4
 
         # A loop for the algorithm
         for i in range(self.iterations):
-
             # Compute the gradient
             tmp_grad = compute_gradient(A, b, y_current)
             grad = y_current - lipinv * tmp_grad
 
             # Apply projection
-            x_next = prox(grad, lambdaLipinv)
+            x_next = scaled_denoiser(grad, self.scaler, self.path_to_model)
 
             tau_new = (1.0 + np.sqrt(1.0 + 4.0 * tau_old ** 2)) / 2.0
 
@@ -58,17 +58,16 @@ class Fista:
             error = compute_function_error(original_img, x_next)
 
             # Break if there is no change in MSE
-            '''
             if error < previous_error:
+                print('Iteration ' + '{}'.format(i + 1) + ' '
+                      + '{:.2f}'.format(error) + ' Yes')
                 previous_error = error
             else:
+                print('Iteration ' + '{}'.format(i + 1) + ' '
+                      + '{:.2f}'.format(error) + ' No')
                 if patience == 0:
                     break
                 else:
                     patience -= 1
-            '''
-            print('Iteration ' + '{}'.format(i + 1) + ' '
-                  + '{:.2f}'.format(error) + (' Yes' if error < previous_error else ' No'))
-            previous_error = error
 
         return x_next, i + 1
